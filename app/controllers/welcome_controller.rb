421bb27d29f4ee33a858b9383aea2bc59b4989a3
Arielle Sullivan <ariellejsullivan@gmail.com>
2020-06-17 20:41:17 -0400

    Manager method

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    if current_user.manager?
+    if current_user&.manager?
       redirect_to team_permit_submissions_path(current_user.team)
     else
       redirect_to permit_submissions_path
