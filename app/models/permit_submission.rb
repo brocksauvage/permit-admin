6d189869e7d845573a70f56a57f344195a26e95e
Arielle Sullivan <ariellejsullivan@gmail.com>
2020-06-21 20:25:44 -0400

    Add knockout button

--- app/models/permit_submission.rb
+++ app/models/permit_submission.rb
@@ -2,7 +2,7 @@ class PermitSubmission < ApplicationRecord
   enum status: %i[accepted filed denied]
   belongs_to :user
   belongs_to :permit_type
-  
+
   alias_attribute :creator, :user
   has_many :permit_documents, dependent: :destroy
   delegate :name, to: :user, prefix: true
