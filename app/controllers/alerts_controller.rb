d258d03cc617720beabd142004435c4b67afe5a5
Babak Sadreddin <sadreddin.babak@gmail.com>
2020-07-24 10:35:54 -0400

    Removed search from alert page

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,14 +1,7 @@
 class AlertsController < ApplicationController
+
   def index
-    @search = alert_index_params.fetch(:search, nil)
     @alerts = Alert.all.order("created_at DESC")
-    @alerts = @alerts.search_by_body(@search) if @search.present?
-  end
-
-  private
-
-  def alert_index_params
-    params.permit(:search)
   end
 
 end
--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,8 +1,5 @@
 class Alert < ApplicationRecord
-  include PgSearch::Model
 
   validates :body, :presence => true
 
-  pg_search_scope :search_by_body, against: :body,
-                  using: { tsearch: { dictionary: 'english' } }
 end
