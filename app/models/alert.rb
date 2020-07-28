7f533b6f4ae1851ad37139eb2b3f142f72370ec9
Babak Sadreddin <sadreddin.babak@gmail.com>
2020-07-28 13:40:54 -0400

    Added validation for link and change time format

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,5 +1,6 @@
 class Alert < ApplicationRecord
 
   validates :body, :presence => true
+  validates_format_of :link, allow_blank: true, :with => /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix, multiline: true
 
 end
